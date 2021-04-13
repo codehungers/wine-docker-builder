fee6948e851ed0f39b0f00a7828f89912425a3e3
codehunger <codehunger@protonmail.com>
2021-04-13 22:26:17 +0300

    add proton-tkg build

--- run-container.sh
+++ run-container.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo docker run -v workdir:/workdir -v $(pwd)/outputs:/mnt/outputs -v $(pwd)/scripts:/usr/local/bin:ro -v $(pwd)/resources:/usr/share/wine-docker-builder:ro -it wine-builder bash
+sudo docker run -v workdir:/workdir -v $(pwd)/outputs:/mnt/outputs -v $(pwd)/scripts:/usr/xbin:ro -v $(pwd)/resources:/usr/share/wine-docker-builder:ro -it wine-docker-builder bash -c 'export PATH=$PATH:/usr/xbin; bash'
