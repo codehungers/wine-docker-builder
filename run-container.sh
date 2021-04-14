b068e4e0e82e738832ceb12358a8bc91580f1de9
codehunger <codehunger@protonmail.com>
2021-04-14 18:47:38 +0300

    Auto remove container when exit so no more zombie containers

--- run-container.sh
+++ run-container.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo docker run -v workdir:/workdir -v $(pwd)/outputs:/mnt/outputs -v $(pwd)/scripts:/usr/xbin:ro -v $(pwd)/resources:/usr/share/wine-docker-builder:ro -it wine-docker-builder bash -c 'export PATH=$PATH:/usr/xbin; bash'
+sudo docker run -v workdir:/workdir -v $(pwd)/outputs:/mnt/outputs -v $(pwd)/scripts:/usr/xbin:ro -v $(pwd)/resources:/usr/share/wine-docker-builder:ro -it  --rm wine-docker-builder bash -c 'export PATH=$PATH:/usr/xbin; bash'
