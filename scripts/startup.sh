ed8b54c52a0b5ddd30a3f4b6b4576b0321af694b
codehunger <codehunger@protonmail.com>
2021-05-18 18:58:13 +0300

    Add to docker sudo command

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -2,3 +2,4 @@ addgroup --gid $GROUPID builder
 adduser --uid $USERID --gid $GROUPID --disabled-password -gecos "" builder
 export PATH=$PATH:/usr/xbin
 > /etc/environment
+echo 'builder ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
