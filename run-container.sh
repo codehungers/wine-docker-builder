a4bb86466995c321c84c7bf39e4434da9afa2ac7
codehunger <codehunger@protonmail.com>
2021-11-06 23:19:29 +0200

    fix running run-container as sudo

--- run-container.sh
+++ run-container.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
+if [[ ! -z "${SUDO_UID}" ]]; then
+    USERID="$SUDO_UID"
+    GROUPID="$SUDO_GID"
+else
+    USERID="$(id -u)"
+    GROUPID="$(id -g)"
+fi
+
 tag=${1:-"latest"}
 mkdir workdir &> /dev/null
 mkdir outputs &> /dev/null
-docker run  -e USERID=$(id -u) \
-            -e GROUPID=$(id -g) \
+docker run  -e USERID=$USERID \
+            -e GROUPID=$GROUPID \
             -v $PWD/outputs:/mnt/outputs \
             -v $PWD/scripts:/usr/xbin:ro \
             -v $PWD/resources:/usr/share/wine-docker-builder:ro \
